2561cc899be7872d7018036931738791b6cff9e1
xypp <2952795729@qq.com>
2024-08-09 08:27:39 +0800

    修复online的筛选问题

--- src/Integration/Online/OnlineAttribute.php
+++ src/Integration/Online/OnlineAttribute.php
@@ -12,7 +12,7 @@ class OnlineAttribute
     public function __invoke(UserSerializer $serializer, User $model, $attributes)
     {
         if ($model->getPreference('discloseOnline') || $serializer->getActor()->can('viewLastSeenAt', $model)) {
-            if (WebsocketUserState::where("path", strval((new ModelPath())->add("online")))->exists()) {
+            if (WebsocketUserState::where("path", strval((new ModelPath())->add("online")))->where("user_id", $model->id)->exists()) {
                 $attributes['onlineState'] = true;
             } else {
                 $attributes['onlineState'] = false;
